8f9484b95508e4446d781dd921971f5c662b8d60
Csenge Fehér <fehercsenge1@gmail.com>
2021-06-17 16:20:56 +0200

    bmi and define-basic-infos tasks added

---
+++ Week-01/Day-04/define-basic-info.ts
@@ -0,0 +1,17 @@
+"use strict";
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+let myName: string = "Csenge Fehér";
+let myAge: number = 25;
+let myHeight: number = 1.66;
+let married: boolean = false;
+
+console.log("Name:", myName);
+console.log("Age:", myAge);
+console.log("Height:", myHeight);
+console.log("Married:", married);
