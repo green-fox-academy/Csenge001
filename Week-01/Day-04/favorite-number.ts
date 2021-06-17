90cbbc4a52f0b3b99c42c56226ed88ac95e94077
Csenge Fehér <fehercsenge1@gmail.com>
2021-06-17 15:34:56 +0200

    favorite-number and swap tasks added

---
+++ Week-01/Day-04/favorite-number.ts
@@ -0,0 +1,7 @@
+export {}
+
+// Store your favorite number in a variable (as a number)
+// And print it like this: 'My favorite number is: 8'
+
+let favoriteNumber: number = 9;
+console.log ("My favorite number is:", favoriteNumber);
---
+++ Week-01/Day-04/swap.ts
@@ -0,0 +1,12 @@
+export {}
+
+// Swap the values of these variables
+
+let numberToSwap1: number = 123;
+let numberToSwap2: number = 526;
+
+numberToSwap1 = 526;
+numberToSwap2 = 123;
+
+console.log(numberToSwap1);
+console.log(numberToSwap2);
