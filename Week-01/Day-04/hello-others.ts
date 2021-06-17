4f75bdd3a6187fde64dc0af398e45665d8eee44f
Csenge Fehér <fehercsenge1@gmail.com>
2021-06-17 13:53:42 +0200

    added hello-others task

---
+++ Week-01/Day-04/hello-others.ts
@@ -0,0 +1,7 @@
+export {}
+
+// Greet 3 of your classmates with this program in three separate lines like:
+
+console.log ("Hello, Robi!");
+console.log("Hello, Norbi!");
+console.log("Hello, Alexa!");
