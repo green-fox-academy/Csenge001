b6352a61433899e40966fd7f512a3c9b75d6d1b0
Csenge Fehér <fehercsenge1@gmail.com>
2021-06-17 14:52:35 +0200

    two-numbers task added

---
+++ Week-01/Day-04/two-numbers.ts
@@ -0,0 +1,9 @@
+export {}
+
+// Create a program that prints a few operations on two numbers: 22 and 13
+
+console.log (22 + 13);
+console.log (22 - 13);
+console.log (22 * 13);
+console.log (22 / 13);
+console.log (22 % 13);
