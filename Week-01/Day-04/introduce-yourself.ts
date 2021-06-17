72b4f7f4556cb072d6dffe0ffa331d6587ff1f0e
Csenge Fehér <fehercsenge1@gmail.com>
2021-06-17 14:43:48 +0200

    introduce-yourself task added

---
+++ Week-01/Day-04/introduce-yourself.ts
@@ -0,0 +1,8 @@
+export {}
+
+// Write a program that prints a few details to the console about you
+// It should print each detail to a new line:
+
+console.log ("Csenge Fehér");
+console.log (25)
+console.log (1.66)
