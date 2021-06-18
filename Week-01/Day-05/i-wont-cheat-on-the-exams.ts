45c5354827ee9505935a1a4e6b1f5db389655037
Csenge Fehér <fehercsenge1@gmail.com>
2021-06-18 13:21:05 +0200

    i-wont-cheat-on-the-exams task added

---
+++ Week-01/Day-05/i-wont-cheat-on-the-exams.ts
@@ -0,0 +1,9 @@
+"use strict";
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+
+for (let a: number = 0; a < 100; a++) {
+    console.log("I won't cheat on the exam!");
+}
