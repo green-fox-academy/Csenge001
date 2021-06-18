af1fb28cef2b4a4d3cec3c080d7fee8aa243a105
Csenge Fehér <fehercsenge1@gmail.com>
2021-06-18 14:46:54 +0200

    fizz-buzz task added

---
+++ Week-01/Day-05/fizz-buzz.ts
@@ -0,0 +1,21 @@
+"use strict";
+
+// Write a program that prints the numbers from 1 to 100
+// but for multiples of three print “Fizz” instead of the number
+// and for the multiples of five print “Buzz”
+// For numbers which are multiples of both three and five print “FizzBuzz”
+
+for (let a: number = 1; a < 101; a++) {
+    if (a % 3 === 0 && a % 5 !== 0) {
+        console.log("Fizz");
+    }
+    if (a % 5 === 0 && a % 3 !== 0) {
+        console.log("Buzz");
+    }
+    if (a % 3 === 0 && a % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+    if (a % 3 !== 0 && a % 5 !== 0) {
+        console.log(a);
+    }
+}
