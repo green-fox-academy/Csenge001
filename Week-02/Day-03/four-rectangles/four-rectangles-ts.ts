236899f2ae6fa00d516b707d79963a50545feef6
Csenge Fehér <fehercsenge1@gmail.com>
2021-06-23 16:46:37 +0200

    four-rectangles task added

---
+++ Week-02/Day-03/four-rectangles/four-rectangles-ts.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+export {};
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Draw four different size and color rectangles
+// Avoid code duplication!
+
+// ctx.rect(10, 10, 100, 100);
+// ctx.fillStyle = "red";
+// ctx.fill();
+
+function drawRectangles (x: number, y: number, width: number, height: number, color: string) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+} 
+    
+drawRectangles(10, 10, 100, 100, "red");
+drawRectangles(450, 100, 50, 250, "purple");
+drawRectangles(50, 150, 100, 20, "pink");
+drawRectangles(300, 100, 10, 50, "brown");
