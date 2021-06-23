c534abdc0d448597a3719b3e4c6c1db5719b61d2
Csenge Fehér <fehercsenge1@gmail.com>
2021-06-23 17:32:59 +0200

    minor changes to upgrade my code

--- Week-02/Day-03/centered-square/centered-square-ts.ts
+++ Week-02/Day-03/centered-square/centered-square-ts.ts
@@ -8,6 +8,10 @@ export {};
 
 // Draw a green 10x10 square to the center of the canvas
 
+const canvasWidth: number = 600;
+const canvasHeight: number = 400;
+const squareSize: number = 10;
+
 ctx.fillStyle = 'green';
-ctx.fillRect(295, 205, 10, 10);
+ctx.fillRect((canvasWidth / 2) - (squareSize / 2), (canvasHeight / 2) - (squareSize / 2), squareSize, squareSize);
 
