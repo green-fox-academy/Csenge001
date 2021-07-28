3ea392c944d92317b7ebd45913d493e154795cae
Csenge Fehér <fehercsenge1@gmail.com>
2021-07-28 17:14:20 +0200

    add classlist-1 task

---
+++ Week-06/Day-03/classlist-1/classlist-1.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+const toBeChanged = document.getElementsByTagName("p");
+
+if (toBeChanged[3].classList.value === "dolphin") {
+    toBeChanged[0].innerHTML = "pear";
+}
+
+if (toBeChanged[0].classList.value === "apple") {
+    toBeChanged[2].innerHTML = "dog";
+}
+
+toBeChanged[0].classList.add("red");
+
+toBeChanged[1].style.borderRadius = 0;
